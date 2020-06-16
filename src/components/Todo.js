dc503c61cb1cf30f96f61683c00c66f5e5e1d662
annedhyacinthe <55201201+annedhyacinthe@users.noreply.github.com>
2020-06-15 22:45:30 -0400

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { connect } from "react-redux";
-import { deleteTask, moveTask } from "../redux/action/actions";
+import { useDispatch } from "react-redux";
 
 const Task = (prop) => {
   console.log(prop);
+  const dispatch = useDispatch();
   return (
     <li key={prop.id}>
       <h3>{prop.content}</h3>
-      <button onClick={() => prop.deleteTask(prop.id)}>Delete</button>
-      <button onClick={() => prop.moveTask(prop.status, prop.id)}>
+      <button onClick={() => dispatch({ type: "DELETE_TODO", id: prop.id })}>
+        Delete
+      </button>
+      <button onClick={() => dispatch({ type: "TOGGLE_TODO", id: prop.id })}>
         {prop.status ? "Incomplete" : "Complete"}
       </button>
     </li>
   );
 };
 
-export default connect(null, {
-  deleteTask: deleteTask,
-  moveTask: moveTask,
-})(Task);
+export default Task;
