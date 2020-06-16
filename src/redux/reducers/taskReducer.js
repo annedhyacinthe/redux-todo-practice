ad61945aa4004bcf814256d7cc796e72530dcf38
annedhyacinthe <55201201+annedhyacinthe@users.noreply.github.com>
2020-06-15 22:42:48 -0400

    Update taskReducer.js

--- src/redux/reducers/taskReducer.js
+++ src/redux/reducers/taskReducer.js
@@ -1,28 +1,28 @@
-import{ ADD_TODO, DELETE_TODO, TOGGLE_TODO } from '../action/actionTypes'
+import { ADD_TODO, DELETE_TODO, TOGGLE_TODO } from "../action/actionTypes";
 
-let nextId = 0
-export default function taskReducer(state = {}, action){
-  switch(action.type){
-    case ADD_TODO:{
-      const content = action.content
-      const newObj = {...state}
-      let k = ++nextId
-      newObj[k]= {content: content,status: false, id:k}
-      return newObj
+let nextId = 0;
+export default function tasks(state = {}, action) {
+  switch (action.type) {
+    case ADD_TODO: {
+      const content = action.content;
+      const newObj = { ...state };
+      let k = ++nextId;
+      newObj[k] = { content: content, status: false, id: k };
+      return newObj;
     }
-    case DELETE_TODO:{
-      const { id } = action.payload
-      const newObj = {...state}
-      delete newObj[id]
-      return newObj
+    case DELETE_TODO: {
+      const { id } = action;
+      let newObj = { ...state };
+      delete newObj[id];
+      return newObj;
     }
-    case TOGGLE_TODO:{
-      const { id } = action.payload
-      const newObj = {...state}
-      newObj[id].status = !newObj[id].status
-      return newObj
+    case TOGGLE_TODO: {
+      const { id } = action;
+      const newObj = { ...state };
+      newObj[id].status = !newObj[id].status;
+      return newObj;
     }
     default:
-      return state
+      return state;
   }
-} 
+}
